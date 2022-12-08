3f3b4cd4601582467bc66ea556e9649c49ac3e91
MORVAN Loïs <lois.morvan@laposte.net>
2022-12-08 17:34:00 +0100

    correction4

--- StarWars.py
+++ StarWars.py
@@ -2,7 +2,7 @@ import requests
 
 def findMovies():
     dataPeople = requests.get('https://swapi.py4e.com/api/people/1')
-    people = dataPeople.json()['films']
+    films = dataPeople.json()['films']
     names = []
     for film_url in films:
         dataFilm = requests.get(film_url)
