eec61c12a28562a04324b6842c3db227cac6f472
MORVAN Loïs <lois.morvan@laposte.net>
2022-12-08 16:55:37 +0100

    ajout des fichiers pour différencier les premiers test des seconds

---
+++ StarWars.py
@@ -0,0 +1,11 @@
+import requests
+
+def findMovies():
+    dataPeople = requests.get('https://swapi.py4e.com/api/people/1')
+    films = dataPeople.json()['films']
+    names = []
+    for film_url in films:
+        dataFilm = requests.get(film_url)
+        film = dataFilm.json()['title']
+        names.append(film)
+    return names
--- hello.py
+++ hello.py
@@ -1,21 +1,6 @@
-import requests
-
 def hello(name):
     if (name == ''):
         raise Exception("The name can not be empty !")
     if (len(name) > 30):
         raise Exception("The name can not have more than 30 characters !")
     return "Hello " + name
-
-def findMovies():
-    urlPeople = 'https://swapi.py4e.com/api/people/?search=Luke'
-    dataPeople = requests.get(urlPeople)
-    people = dataPeople.json()
-    films = people['results'][0]['films']
-    names = []
-    for i in range(len(films)):
-        urlFilm = people['results'][0]['films'][i]
-        dataFilm = requests.get(urlFilm)
-        film = dataFilm.json()
-        names.append(film['title'])
-    return names
