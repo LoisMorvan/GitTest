2b813479ec4df8872de71a8c1ad15cb1b660a807
MORVAN Loïs <lois.morvan@laposte.net>
2022-12-08 12:15:27 +0100

    exeption

--- test_hello.py
+++ test_hello.py
@@ -2,5 +2,14 @@ import hello
 
 def test_hello():
     assert hello.hello('Loïs') == 'Hello Loïs'
-    assert hello.hello('') == Exception
-    assert hello.hello('SuperSuperSuperSuperSuperSuper') == Exception
+    try:
+        hello.hello('')
+        assert False
+    except Exception:
+        assert True
+
+    try:
+        hello.hello('SuperSuperSuperSuperSuperSuper')
+        assert False
+    except Exception:
+        assert True
